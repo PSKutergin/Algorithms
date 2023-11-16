7bd7e52d66df7bfe2e638353f9e119c8381e3e30
PSKutergin <PSKutergin@gmail.com>
2023-11-16 09:13:27 +0300

    search max in array

---
+++ ArrayMax.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class ArrayMax {
+    public static void main(String[] args) {
+        int[] ages = {10, 6, 15, 93, 42, 7, 32};
+        
+        // Поиск максимума через сортировку массива
+        /* Arrays.sort(ages);
+        int maxAge = ages[ages.length - 1]; */
+
+        // Поиск максимума через сравнение каждого элемента массива
+        int maxAge = 0;
+        for(int i = 0; i < ages.length; i++) {
+            maxAge = Math.max(maxAge, ages[i]);
+        }
+
+        System.out.println(maxAge);
+    }
+}
